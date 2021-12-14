a61ebe6cd803e525769836d2167efcd5b513c897
Aiman Farhan <aimanfaruk98@gmail.com>
2021-12-15 00:06:58 +0800

    main hbs sidebar

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
     ensureAuth: function (req,res, next) {
-        if(req.isAuthenthicated()){
+        if(req.isAuthenticated()){
             return next()
         } else {
             res.redirect("/")
         }
     },
     ensureGuest: function (req,res, next){
-        if (req.isAuthenthicated()){
+        if (req.isAuthenticated()){
             res.redirect("/dashboard")
         } else {
             return next()
